5b6085e28e9226c1230511a96aa36004c9a43f34
Dr-Alex-Dr <mr.lopin900@gmail.com>
2024-03-07 20:05:18 +0500

    fromEvent

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Observable, debounceTime, distinctUntilChanged, fromEvent, map } from 'rxjs';
+import { Observable, debounceTime, distinctUntilChanged, fromEvent, map, scan, throttleTime } from 'rxjs';
 
 @Component({
   selector: 'app-search',
@@ -14,29 +14,38 @@ export class SearchComponent {
   }
 
   ngOnInit() {
-    const search$ = new Observable<Event>(observer => {
-      console.log('Start in observable');
-      const search = document.querySelector('#Search');
-      search?.addEventListener('input', (event) => {
-        observer.next(event);
-      })
+    // const search$ = new Observable<Event>(observer => {
+    //   console.log('Start in observable');
+    //   const search = document.querySelector('#Search');
+    //   search?.addEventListener('input', (event) => {
+    //     observer.next(event);
+    //   })
 
-      console.log('End in observable');
-    });
+    //   console.log('End in observable');
+    // });
 
-   
-    search$
+    fromEvent<MouseEvent>(document, 'click')
     .pipe(
-      map(event => {
-        return (event.target as HTMLInputElement).value;
-      }),
-      debounceTime(500),
-      map(value => value.length > 3 ? value : ''),
-      distinctUntilChanged()
-      )
-    .subscribe(value => {
-      console.log(value)
-    });
+      throttleTime(1000),
+      map((event) => event.clientX),
+      scan((count, clientX) => count + clientX, 0))
+    .subscribe((count) => {
+      console.log(count);
+    })
+
+
+    // search$
+    // .pipe(
+    //   map(event => {
+    //     return (event.target as HTMLInputElement).value;
+    //   }),
+    //   debounceTime(500),
+    //   map(value => value.length > 3 ? value : ''),
+    //   distinctUntilChanged()
+    //   )
+    // .subscribe(value => {
+    //   console.log(value)
+    // });
   }
 
   example: string = 'Example';
